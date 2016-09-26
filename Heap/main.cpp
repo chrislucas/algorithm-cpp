c348559a51cbb40775ce68cfd47222b61bdabfb5
lucaschris <crhistoffer.luccas@gmail.com>
2016-09-26 13:25:15 -0300

    correcao do algoitmo de heapifyup

--- Heap/main.cpp
+++ Heap/main.cpp
@@ -81,19 +81,19 @@ BinaryHeap<T>::BinaryHeap() {
 template<class T>
 T BinaryHeap<T>::left(int parent) {
     int idx = 2 * parent + 1;
-    return idx < _heap.size() ? idx : INF;
+    return idx < _heap.size() ? idx : -1;
 }
 
 template<class T>
 T BinaryHeap<T>::right(int parent) {
     int idx = 2 * parent + 2;
-    return idx < _heap.size() ? idx : INF;
+    return idx < _heap.size() ? idx : -1;
 }
 
 template<class T>
 T BinaryHeap<T>::parent(int child) {
     int idx = (child - 1) / 2;
-    return child == 0 ? INF : idx;
+    return child == 0 ? -1 : idx;
 }
 
 
@@ -167,9 +167,9 @@ void BinaryHeap<T>::heapifyup(int idx) {
         int hi = _heap[idx];
         if(hp > hi) {
             int tmp = _heap[idx];
-            _heap[idx] = _heap[hp];
-            _heap[hp] = tmp;
-            heapifyup(hp);
+            _heap[idx] = _heap[p];
+            _heap[p] = tmp;
+            heapifyup(p);
         }
     }
     return;
@@ -209,7 +209,7 @@ void run_1() {
 
 int main()
 {
-    cout<< INF <<" " << ((1<<31)-1) << endl ;
+    //cout<< INF <<" " << ((1<<31)-1) << endl ;
     run_1();
     return 0;
 }
