d01395d655b155d4a330dc5a4ee72e0aedc72c76
wiselabs <christoffer.luccas@gmail.com>
2016-11-28 09:39:46 -0200

    novas funcoes de teste

--- judges/spoj/br/SPOJBR_ISOSCELES_1/main.cpp
+++ judges/spoj/br/SPOJBR_ISOSCELES_1/main.cpp
@@ -18,7 +18,6 @@ public:
 
 Point2D::Point2D(ll x, ll y) : x(x), y(y) {}
 
-
 class Edge {
 public:
     ll xs, ys, xe, ye;
@@ -37,70 +36,80 @@ public:
     triangulo isosceles
 */
 
+void solver1Test(int q);
+void solver2Test();
+void solver3Test();
+
 int main()
 {
+    /*
     int qPoints;
-    vector<Point2D> points;
-    while(scanf("%d", &qPoints) && qPoints > 0) {
-        ll x, y;
-        for(int i=0; i<qPoints; i++){
-            scanf("%lld %lld", &x, &y);
-            Point2D point2D(x, y);
-            points.push_back(point2D);
-        }
-        ll distances [1111], answer = 0;
-        for(int i=0; i<qPoints; i++) {
-            for(int j=0; j<qPoints; j++) {
-                Edge edge(points[i].x, points[i].y, points[j].x, points[j].y);
-                distances[j] = edge.euclidianDistance();
-            }
-            sort(distances, distances+qPoints);
-            long long counter = 1;
-            distances[qPoints] = -1;
-            for(int j=1; j<=qPoints; j++) {
-                if(distances[j] != distances[j-1]) {
-                    /*
-                        A combinacao de pontos ABC, BCA, CBA ... formam o mesmo triangulo
-                        A aresta AB e BA sao as mesmas, para evitar que seja contada 2x fazemos a conta abaixo
-                    */
-                    answer += counter*(counter-1)/2;
-                    counter = 1;
-                }
-                else {
-                    counter++;
-                }
-            }
-        }
-        printf("%lld\n", answer);
-    }
+    while(scanf("%d", &qPoints) && qPoints > 0)
+        solver1Test(qPoints);
+    */
+    solver2Test();
     return 0;
+}
+
+void solver1Test(int qPoints) {
+    ll x, y;
+    vector<Point2D> points;
+    for(int i=0; i<qPoints; i++){
+        scanf("%lld %lld", &x, &y);
+        Point2D point2D(x, y);
+        points.push_back(point2D);
+    }
+    ll distances [1111], answer = 0;
+    for(int i=0; i<qPoints; i++) {
+        for(int j=0; j<qPoints; j++) {
+            Edge edge(points[i].x, points[i].y, points[j].x, points[j].y);
+            distances[j] = edge.euclidianDistance();
+        }
+        sort(distances, distances+qPoints);
+        long long counter = 1;
+        distances[qPoints] = -1;
+        for(int j=1; j<=qPoints; j++) {
+            if(distances[j] != distances[j-1]) {
+                /*
+                    A combinacao de pontos ABC, BCA, CBA ... formam o mesmo triangulo
+                    A aresta AB e BA sao as mesmas, para evitar que seja contada 2x fazemos a conta abaixo
+                */
+                answer += counter*(counter-1)/2;
+                counter = 1;
+            }
+            else {
+                counter++;
+            }
+        }
+    }
+    printf("%lld\n", answer);
 }
 
 void solver2Test() {
     int qPoints;
     while(scanf("%d", &qPoints) && qPoints > 0) {
-        ll p2Dx[1024], p2Dy[1024];
+        ll p2Dx[1111], p2Dy[1111];
         for(int i=0; i<qPoints; i++) {
-            scanf("%lld %lld", p2Dx[i], p2Dy[i]);
+            scanf("%lld %lld", &p2Dx[i], &p2Dy[i]);
         }
-        ll distances [1024], answer = 0;
+        ll distances [1111], answer = 0;
         for(int i=0; i<qPoints; i++) {
-            for(int j=i+1; j<qPoints; j++) {
+            for(int j=0; j<qPoints; j++) {
                 Edge edge(p2Dx[i], p2Dy[i], p2Dx[j], p2Dy[j]);
                 distances[j] = edge.euclidianDistance();
             }
-            sort(distances/*+i*/, distances+qPoints);
+            sort(distances, distances+qPoints);
             long long counter = 0;
             distances[qPoints] = -1;
-            for(int j=0; j<qPoints-4; j++) {
+            for(int j=0; j<qPoints; j++) {
                 if(distances[j+1] != distances[j]) {
-
+                    answer += counter*(counter-1)/2;
+                    counter = 0;
                 }
-                else {
-
-                }
+                counter++;
             }
         }
+        printf("%lld\n", answer);
     }
     return;
 }
